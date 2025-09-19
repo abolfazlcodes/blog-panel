eb88282bb04eb74b20ac59e70fb05dc1f50403b8
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2025-09-19 14:55:04 +0330

    feat: complete login process and create token for user - #1

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import { router as authRouter } from "./src/routes/auth.js";
+import { ICustomError } from "./src/types/index.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -16,6 +20,21 @@ app.get("/", (req, res) => {
     data: [{ id: 1, title: "first blog" }],
   });
 });
+
+// general error handler middleware;
+app.use(
+  (error: ICustomError, req: Request, res: Response, next: NextFunction) => {
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+
+    res.status(status).json({
+      message,
+      error: error,
+      data: data,
+    });
+  }
+);
 
 app.listen(8080, () => {
   console.log("Backend running on http://localhost:8080");
