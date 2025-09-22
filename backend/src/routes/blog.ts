2cb011cddcd7325c6f42e699f077d5d67e36833d
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2025-09-22 21:46:27 +0330

    feat: add update blog route handler

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -6,6 +6,7 @@ import {
   createBlogHandler,
   deleteBlogHandler,
   getAllBlogsHandler,
+  updateBlogHandler,
 } from "../controllers/blog.js";
 
 const router = express.Router();
@@ -44,7 +45,37 @@ router.post(
   createBlogHandler
 );
 
-router.get("/blogs/:id", () => {});
+router.put(
+  "/blogs/:id",
+  [
+    body("title")
+      .trim()
+      .notEmpty()
+      .withMessage("title is required")
+      .isLength({
+        min: 5,
+      })
+      .withMessage("title should be at least 5 characters"),
+    body("short_description")
+      .trim()
+      .notEmpty()
+      .withMessage("short description is required")
+      .isLength({
+        min: 5,
+      })
+      .withMessage("short description should be at least 5 characters"),
+    body("description")
+      .trim()
+      .notEmpty()
+      .withMessage("description is required")
+      .isLength({
+        min: 10,
+      })
+      .withMessage("description should be at least 10 characters"),
+  ],
+  errorValidator,
+  updateBlogHandler
+);
 
 router.delete("/blogs/:id", deleteBlogHandler);
 
