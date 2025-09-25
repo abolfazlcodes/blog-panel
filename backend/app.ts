0c202a1093e58800cb60ee6ac18a611b8cadefec
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2025-09-25 22:35:34 +0330

    refactor media file and serve uploaded files statically

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,19 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import { router as authRouter } from "./src/routes/auth.js";
 import { router as blogRouter } from "./src/routes/blog.js";
 import { router as mediaFileRouter } from "./src/routes/media-file.js";
 import { ICustomError } from "./src/types/index.js";
+
+// make sure __dirname is defined for ES modules
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// serve uploads folder
 
 dotenv.config();
 
@@ -23,7 +31,10 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(express.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "src/uploads")));
 
 app.use(authRouter);
 app.use(blogRouter);
