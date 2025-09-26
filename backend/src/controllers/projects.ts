724f57fb6c802fb0407330267407f9330dabd956
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2025-09-26 07:55:15 +0330

    feat: add project create document route and logic

--- backend/src/controllers/projects.ts
+++ backend/src/controllers/projects.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Response, Request } from "express";
+import slugify from "slugify";
+
 import prisma from "../prisma.js";
 
 import CustomError from "../utils/customError.js";
@@ -28,6 +30,78 @@ export const getAllProjectsHandler = async (
     res.status(HTTP_STATUS_CODES.StatusOk).json({
       message: "successful",
       data: allProjects,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createProjectHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // @ts-ignore
+  const userId = req?.userId;
+
+  const { title, short_description, description, content, cover_image } =
+    req.body;
+
+  try {
+    const slug = slugify.default(title, {
+      lower: true,
+    });
+
+    if (!slug) {
+      const error = new CustomError(
+        "Something went wrong. Could not create slug. Try again later."
+      );
+      error.statusCode = HTTP_STATUS_CODES.StatusInternalServerError;
+      throw error;
+    }
+
+    // check if slug already exists
+    const isSlugAlreadyExists = await prisma.project.findUnique({
+      where: {
+        slug,
+      },
+    });
+
+    if (isSlugAlreadyExists) {
+      const error = new CustomError(
+        "Project with the same slug already exists. Please try a different title"
+      );
+      error.statusCode = HTTP_STATUS_CODES.StatusBadRequest;
+      throw error;
+    }
+
+    const newProject = {
+      title,
+      slug,
+      short_description,
+      description,
+      cover_image,
+      content,
+      is_draft: true,
+      userId: userId, // the logged in userId
+      published_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    };
+
+    const result = await prisma.project.create({
+      data: newProject,
+    });
+
+    if (!result) {
+      const error = new CustomError(
+        "Something went wrong. Could not create project. Try again later."
+      );
+      error.statusCode = HTTP_STATUS_CODES.StatusInternalServerError;
+      throw error;
+    }
+
+    res.status(HTTP_STATUS_CODES.StatusCreated).json({
+      message: "Project was created successfully",
     });
   } catch (error) {
     next(error);
--- backend/src/routes/projects.ts
+++ backend/src/routes/projects.ts
@@ -4,10 +4,46 @@ import { body } from "express-validator";
 import { errorValidator } from "../middlewares/validator.js";
 import { isAuthenticatedValidator } from "../middlewares/isAuth.js";
 
-import { getAllProjectsHandler } from "../controllers/projects.js";
+import {
+  createProjectHandler,
+  getAllProjectsHandler,
+} from "../controllers/projects.js";
 
 const router = express.Router();
 
-router.get("/projects", isAuthenticatedValidator, getAllProjectsHandler);
+router.get("/project", isAuthenticatedValidator, getAllProjectsHandler);
+
+router.post(
+  "/project",
+  isAuthenticatedValidator,
+  [
+    body("title")
+      .trim()
+      .notEmpty()
+      .withMessage("title is required")
+      .isLength({
+        min: 5,
+      })
+      .withMessage("title should be at least 5 characters"),
+    body("short_description")
+      .trim()
+      .notEmpty()
+      .withMessage("short description is required")
+      .isLength({
+        min: 5,
+      })
+      .withMessage("short description should be at least 5 characters"),
+    body("description")
+      .trim()
+      .notEmpty()
+      .withMessage("description is required")
+      .isLength({
+        min: 10,
+      })
+      .withMessage("description should be at least 10 characters"),
+  ],
+  errorValidator,
+  createProjectHandler
+);
 
 export { router };
